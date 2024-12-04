1f70068fe09f5e46bc73cbc790435d7bf17d760c
RuslanDiachenko <diachenkorp@gmail.com>
2024-12-04 21:47:30 +0200

    Task_4 remake to simple known.

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -4,14 +4,19 @@ using namespace std;
 
 int main() {
     // Init
-    double minSalary{1000}, maxSalary{1000000};
-    double userSalary{0};
+    int minSalary{1000}, maxSalary{1000000};
+    int userSalary{0};
 
     // Main
     // NOT USE -> else, ==, <=, >=, &&, ||
     cout << "Enter Your Salary: ";
     cin >> userSalary;
 
-    cout << ( userSalary < maxSalary ? (userSalary < minSalary ? "Work More!" : "You're Good!") : "Wow, You're millionaire!" ) << endl;
+    if (userSalary < minSalary) cout << "Work More!" << endl;
+    if (userSalary > minSalary - 1)
+    {
+        if (userSalary < maxSalary - 1) cout << "You're Good!" << endl;
+        if (userSalary > maxSalary - 1) cout << "Wow, You're millionaire!" << endl;
+    }
     return 0;
 }
